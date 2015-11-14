43c9ccadec65f427d89cc3a6aee6f43f69273604
Takeshi KOMIYA <i.tkomiya@gmail.com>
2015-11-14 19:38:16 +0900

    Initialize project

---
+++ README.rst
@@ -0,0 +1,17 @@
+sphinxcontrib-apiblueprint
+==========================
+
+`sphinxcontrib-apiblueprint` is Sphinx extension to create API docs using `API Blueprint`_
+
+.. _API Blueprint: https://apiblueprint.org/
+
+Usage
+-----
+
+Include this extension in conf.py::
+
+    extensions = ['sphinxcontrib.apiblueprint']
+
+Write ``apiblueprint`` directive into reST file where you want to import API doc::
+
+    .. swaggerdoc:: [path to API blueprint definition file]
---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+long_desc = open('README.rst').read()
+
+requires = ['Sphinx>=0.6', 'recommonmark']
+
+setup(
+    name='sphinxcontrib-apiblueprint',
+    version='0.0.1',
+    url='https://github.com/tk0miya/sphinxcontrib-apiblueprint',
+    license='BSD',
+    author='Takeshi KOMIYA',
+    author_email='i.tkomiya@gmail.com',
+    description='Sphinx extension to create API docs using API Blueprint',
+    long_description=long_desc,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Documentation',
+        'Topic :: Documentation :: Sphinx',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+)
